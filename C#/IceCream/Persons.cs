42eca64ba382aa6f76e342e3762baf2c80aeff4e
unknown <sethbarrett01@gmail.com>
2022-05-24 18:29:07 -0400

    Finishing C# implementation

---
+++ C#/IceCream/Persons.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCream
+{
+    class Persons
+    {
+        private static int count = 0;
+        private string name;
+        public string Name { get => name; set => name = value; }
+        private int cash;
+        public int Cash 
+        { 
+            get => cash; 
+            set => cash = (value > 9999 || value <= 0) ? value : throw new ArgumentException("Cash must be between $1 and $9999"); 
+        }
+        public Persons(string name = "Person")
+        {
+            Name = name;
+            count++;
+        }
+        public decimal FindPortion(int gallonG, int cashT)
+        {
+            return ((decimal)(cash) / (decimal)(cashT)) * (decimal)gallonG;
+        }
+    }
+}
