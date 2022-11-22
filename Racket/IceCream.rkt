d239f0beb75ed877addc55f23eb6382b00443bb8
Seth <sethbarrett01@gmail.com>
2022-11-22 09:19:19 -0500

    Added Racket IceCream implementation and updated ReadMe

---
+++ Racket/IceCream.rkt
@@ -0,0 +1,95 @@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##BBGGGPPPPPPPPGGGBB##&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BPGGPP555555555555555555555555PPGGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@&B5?~:..:~7Y5555555555555555555555555555555PGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@#57^...........^7Y55555555555555555555555555555555PGB&@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@#Y!:.................^?Y555555555555555555555555555555555PB&@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@&5!.......................:!J5555555555555555555555555555555555PB&@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@&BPJ7~:........................^?5P555555555555555555555555555555555G&@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@&BPPPGGP5J7^.......................:7Y5555555555555555555555555555555555G#@@@@@@@@@@@@@
+; @@@@@@@@@@@&BPPPPPPPPPGGP5?~:.....................:!Y5555555555555555555555555555555555G&@@@@@@@@@@@
+; @@@@@@@@@@#PPPPPPPPPPPPPPGGP5?~:....................:!Y5555555555555555555555555555555555B@@@@@@@@@@
+; @@@@@@@@&GPPPPPPPPPPPPPPPPPPGGP57^....................:!YP55555555555555555555555555555555P#@@@@@@@@
+; @@@@@@@#PPPPPPPPPPPPPPPPPPPPPPPGGPJ!:...................:7Y555555555555555555555555555555555B@@@@@@@
+; @@@@@@BPPPPPPPPPPPPPPPPPPPPPPPPPPPGG57:...................:7555555555555555555555555555555555G@@@@@@
+; @@@@@BPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGGP?^...................^J55555555555555555555555555555555G@@@@@
+; @@@@BPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGPJ^...................~Y5555555555555555555555555555555G@@@@
+; @@@#PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGGY~....................:75555555555555555555555555555555B@@@
+; @@&PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGGY!........................^J555555555555555555555555555555#@@
+; @@GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPG5!...........................:755555555555555555555555555555P@@
+; @#PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGP7:..............................^J5555555555555555555555555555#@
+; @GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGGJ^.................................:7555555555555555555555555555P@
+; &PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPG5!.................!!..................~Y55555555555555555555555555&
+; #PPPPPPPPPPPPPPPPPPPPPPPPPPPPGPJ:................~YGGJ:.................:J5555555555555555555555555#
+; #PPPPPPPPPPPPPPPPPPPPPPPPPPPG5!................^JPGPPG5~..................7555555555555555555555555B
+; #PPPPPPPPPPPPPPPPPPPPPPPPPPGJ:................7PGPPPPPGP7..................!55555555555555555555555G
+; #PPPPPPPPPPPPPPPPPPPPPPPPGP7................^YGPPPPPPPPPGJ:.................^Y555555555555555555555B
+; #PPPPPPPPPPPPPPPPPPPPPPPG5~................7PGPPPPPPPPPPPG5^.................^Y55555555555555555555#
+; &GPPPPPPPPPPPPPPPPPPPPPGY:...............:JGPPPPPPPPPPPPPPGP~.................:J5555555555555555555&
+; @GPPPPPPPPPPPPPPPPPPPPG?:...............^5GPPPPPPPPPPPPPPPPGP!.................:J55555555555555555P@
+; @#PPPPPPPPPPPPPPPPPPPP7................!PGPPPPPPPPPPPPPPPPPPPP7..................?5555555555555555#@
+; @@GPPPPPPPPPPPPPPPPPP!................7PPPPPPPPPPPPPPPPPPPPPPPP7..................?55555555555555P@@
+; @@&PPPPPPPPPPPPPPPGP~................?GPPPPPPPPPPPPPPPPPPPPPPPPG7.................:J5555555555555#@@
+; @@@#PPPPPPPPPPPPPGP~................JGPPPPPPPPPPPPPPPPPPPPPPPPPPP!.................:J55555555555B@@@
+; @@@@BPPPPPPPPPPPGP~...............:JGPPPPPPPPPPPPPPPPPPPPPPPPPPPPP~.................:Y555555555G@@@@
+; @@@@@BPPPPPPPPPPP~................JGPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGP^.................^55555555G@@@@@
+; @@@@@@BPPPPPPPPP!................?GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPG5:.................!555555G@@@@@@
+; @@@@@@@#PPPPPPG7................?GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGJ..................?5555B@@@@@@@
+; @@@@@@@@&GPPPG?................!GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPG7.................:Y5P&@@@@@@@@
+; @@@@@@@@@@#PPY:...............^PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP~................ ~B@@@@@@@@@@
+; @@@@@@@@@@@&B^ ..............:5GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGY:...............7#@@@@@@@@@@@
+; @@@@@@@@@@@@@B?:.............?GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPG7.............7B@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@#J^..........~PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP^.........:?B@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@&5!:......:YGPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGJ.......!5&@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@#Y!:...!GPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP^ .:~YB@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@#57~5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGJ75#@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&#BBGGPPPPPPPPPPPPPPPPPPPPPPPPGGBB#&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+; @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&##BBBBGGGGGGGGBBBB##&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#lang racket
+
+(define Person% (class object%
+    (super-new)
+    (init name cash portion)
+    (define __name name)
+    (define __cash cash)
+    (define __portion portion)
+    (define/public (get-name) __name)
+    (define/public (get-cash) __cash)
+    (define/public (set-name name) 
+        (set! __name name))
+    (define/public (set-cash cash) 
+        (set! __cash cash))
+    (define/public (set-portion totalCash totalGal)
+         (set! __portion (* (/ __cash totalCash) totalGal)))
+    (define/public (display-info)
+        (printf "Name: ~a\n" __name)
+        (printf "Cash: ~a\n" __cash)
+        (printf "Portion: ~a\n\n" __portion))
+))
+
+(define people (list))
+(define totalCash 0)
+
+
+(for ([i (in-naturals)])
+    (printf "Add person?(y/n)\n")
+    #:break (equal? (read-line) "n")
+    (printf "Name: " )
+    (define name (read-line))
+    (printf "Cash Thrown: $")
+    (define cash (read-line))
+    (set! cash (string->number cash))
+    (set! people (append people (list (new Person% (name name) (cash cash) (portion 0)))))
+    (set! totalCash (+ totalCash cash))
+)
+
+(printf "Total gallons of ice cream acquired: ")
+(define totalGal (string->number (read-line)))
+
+(for ([i (length people)])
+    (define peep (list-ref people i))
+    (send peep set-portion totalCash totalGal))
+
+(for ([i (length people)])
+    (define peep (list-ref people i))
+    (send peep display-info))
